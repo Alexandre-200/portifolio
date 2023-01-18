667e3d52c8fa8e7b7da2a7f6d8a3313695dd8221
Alexandre-200 <alexandre_200@outlook.com.br>
2023-01-18 11:06:31 -0300

    update

--- js/script.js
+++ js/script.js
@@ -1,22 +1,11 @@
 const mario = document.querySelector(".mario");
+const quadro_1 = document.querySelector(".quadro-1");
 const cloud = document.querySelector(".cloud");
 
-function reiniciar() {
-  window.location.reload();
-}
-
-const jump = () => {
-  mario.classList.add("jump");
-
-  setTimeout(() => {
-    mario.classList.remove("jump");
-  }, 500);
-};
 
 const loop = setInterval(() => {
   const cloudPosition = cloud.offsetLeft;
 
-  
   const marioPosition = +window
     .getComputedStyle(mario)
     .bottom.replace("px", "");
@@ -33,15 +22,28 @@ const loop = setInterval(() => {
     mario.style.bottom = `${marioPosition}px`;
 
     mario.src = "./img/game-over.png";
-    mario.style.width = "75px";
-    
+
     clearInterval(loop);
   }
   mario.style.marginLeft = "50px";
 }, 10);
 
 document.addEventListener("keydown", (e) => {
-  if (e.code === "ArrowRight") {
-    alert("ok");
+  if (e.code === "ArrowLeft") {
+    let position = +window.getComputedStyle(quadro_1).right.replace("px", "");
+   
+    position -= 5;
+    quadro_1.style.right = `${position}px`;
+  } else if (e.code === "ArrowUp") {
+    alert("set U");
+  } else if (e.code === "ArrowRight") {
+    let position = +window.getComputedStyle(quadro_1).right.replace("px", "");
+   
+    position += 5;
+    quadro_1.style.right = `${position}px`;
+  } else if (e.code === "ArrowDown") {
+    alert("set D");
   }
 });
+
+document.addEventListener("keyup", (e) => {});
